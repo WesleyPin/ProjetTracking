59bf81020844a87c12a6b5062c3003a1d0361438
Wesley PIN <wpin@itop.fr>
2018-12-11 22:08:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wpin/projettracking/PageAccueil.java
@@ -0,0 +1,13 @@
+package com.example.wpin.projettracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageAccueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_accueil);
+    }
+}
