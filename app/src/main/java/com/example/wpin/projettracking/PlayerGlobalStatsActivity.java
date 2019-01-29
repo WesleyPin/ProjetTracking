28c9c92f41f7af6eed6d3fce5e9a661e3fc3468e
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-01-29 12:16:51 +0100

    Ajout activité PlayerGlobalStatsActivity

---
+++ app/src/main/java/com/example/wpin/projettracking/PlayerGlobalStatsActivity.java
@@ -0,0 +1,15 @@
+package com.example.wpin.projettracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerGlobalStatsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_global_stats);
+    }
+}
