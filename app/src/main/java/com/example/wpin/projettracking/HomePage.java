9283e7dcc18b99e2ebf5af0c9fb136efd23e109f
Wesley PIN <wpin@itop.fr>
2019-01-29 18:43:32 +0100

    Ajout de la fonctionnalité de recherche, appel à l'API pour récupérer les informations du joueur (Juste niveau pour l'instant).

--- app/src/main/java/com/example/wpin/projettracking/HomePage.java
+++ app/src/main/java/com/example/wpin/projettracking/HomePage.java
@@ -1,7 +1,11 @@
 package com.example.wpin.projettracking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void onSearch(View v) {
+        TextView textPseudo = findViewById(R.id.tvPseudo);
+        Spinner spinRegion = findViewById(R.id.spRegion);
+        Intent i = new Intent(this, PlayerGlobalStatsActivity.class);
+        i.putExtra("Pseudo", textPseudo.getText().toString());
+        i.putExtra("Region", spinRegion.getSelectedItem().toString());
+        startActivityForResult(i,100);
     }
 }
--- app/src/main/java/com/example/wpin/projettracking/PlayerGlobalStatsActivity.java
+++ app/src/main/java/com/example/wpin/projettracking/PlayerGlobalStatsActivity.java
@@ -2,6 +2,11 @@ package com.example.wpin.projettracking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class PlayerGlobalStatsActivity extends AppCompatActivity
 {
@@ -11,5 +16,23 @@ public class PlayerGlobalStatsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_global_stats);
+
+        // Récupération du pseudo et de la région choisit par le joueur/utilisateur.
+        String pseudo = getIntent().getStringExtra("Pseudo");
+        String region = getIntent().getStringExtra("Region");
+        TextView titrePseudoRegion = findViewById(R.id.tvPlayerName);
+        titrePseudoRegion.setText(pseudo + " - " + region);
+
+        // Appel à l'API de LoL pour récupérer les informations du joueur. (Niveau pour l'instant)
+        Ion.with(this)
+                .load("https://" + region + "1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + pseudo + "?api_key=RGAPI-b763a53e-0613-44da-abc1-37aa4acd3dd0")
+                .asJsonObject()
+                .setCallback(new FutureCallback<JsonObject>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonObject result) {
+                        TextView niveau = findViewById(R.id.tvPlayerLevel);
+                        niveau.setText("Niveau d'invocateur : " + result.get("summonerLevel").getAsString());
+                    }
+                });
     }
 }
