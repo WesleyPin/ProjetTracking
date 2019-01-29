0881b4f1b4cbea8605a874334f05494ba7bae6a3
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-01-29 11:58:20 +0100

    Rename de la classe HomePage

--- app/src/main/java/com/example/wpin/projettracking/HomePage.java
+++ app/src/main/java/com/example/wpin/projettracking/HomePage.java
@@ -3,11 +3,11 @@ package com.example.wpin.projettracking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PageAccueil extends AppCompatActivity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_accueil);
+        setContentView(R.layout.activity_home_page);
     }
 }
