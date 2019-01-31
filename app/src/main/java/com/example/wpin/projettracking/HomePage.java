6b36e703fd7eee0549a766cfa35826db6e77f516
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-01-31 21:02:21 +0100

    Refactor des noms de variables + quelques fonctionnalités (l'appli fonctionne si la clé est à jour)

--- app/src/main/java/com/example/wpin/projettracking/HomePage.java
+++ app/src/main/java/com/example/wpin/projettracking/HomePage.java
@@ -4,23 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
+
+    EditText etSearchPlayer;    // Input pour entrer un nom d'invocateur
+    Spinner spinRegion;         // Select des différentes régions
+    Intent searchingIntent;     // Intention de recherche
+    Button btnSearch;           // Bouton "Rechercher"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        btnSearch = findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {   // Quand on clique sur Rechercher
+                search();  // On appelle la méthode search()
+            }
+        });
     }
 
-    public void onSearch(View v) {
-        TextView textPseudo = findViewById(R.id.tvPseudo);
-        Spinner spinRegion = findViewById(R.id.spRegion);
-        Intent i = new Intent(this, PlayerGlobalStatsActivity.class);
-        i.putExtra("Pseudo", textPseudo.getText().toString());
-        i.putExtra("Region", spinRegion.getSelectedItem().toString());
-        startActivityForResult(i,100);
+    public void search() {
+        etSearchPlayer = findViewById(R.id.etSearchPlayer);
+        spinRegion = findViewById(R.id.spRegion);
+
+        searchingIntent = new Intent(this, PlayerGlobalStatsActivity.class);    // On précise l'activité à lancer
+
+        String strSearchedUsername = etSearchPlayer.getText().toString();  // Récupération du nom de joueur rentré par l'utilisateur
+        searchingIntent.putExtra("searchedUsername", strSearchedUsername);  // On met en extra strSearchedUsername pour pouvoir le récupérer dans l'autre activité
+
+        String strRegion = spinRegion.getSelectedItem().toString(); // Récupération de la région sélectionnée
+        searchingIntent.putExtra("searchedRegion", strRegion);
+        startActivity(searchingIntent);
     }
 }
--- app/src/main/java/com/example/wpin/projettracking/PlayerGlobalStatsActivity.java
+++ app/src/main/java/com/example/wpin/projettracking/PlayerGlobalStatsActivity.java
@@ -2,6 +2,9 @@ package com.example.wpin.projettracking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonObject;
@@ -10,6 +13,7 @@ import com.koushikdutta.ion.Ion;
 
 public class PlayerGlobalStatsActivity extends AppCompatActivity
 {
+    final static String API_KEY = "RGAPI-1a661ebc-0da3-4d90-9068-6253b9f6d022"; // à remplacer si deprecated
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,22 +21,90 @@ public class PlayerGlobalStatsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_global_stats);
 
-        // Récupération du pseudo et de la région choisit par le joueur/utilisateur.
-        String pseudo = getIntent().getStringExtra("Pseudo");
-        String region = getIntent().getStringExtra("Region");
-        TextView titrePseudoRegion = findViewById(R.id.tvPlayerName);
-        titrePseudoRegion.setText(pseudo + " - " + region);
+        // Récupération du username et de la région recherchés par l'utilisateur.
+        String strSearchedUsername = getIntent().getStringExtra("searchedUsername");
+        String strSearchedRegion = getIntent().getStringExtra("searchedRegion");
+
+        String strRegion = getRegionName(strSearchedRegion);    // Récupération du nom de la région à mettre dans l'URL de requête
+
+        TextView tvPlayerRegion = findViewById(R.id.tvPlayerRegion);
+        tvPlayerRegion.setText(strSearchedRegion);  // On remplace "Région" par la région du joueur
 
         // Appel à l'API de LoL pour récupérer les informations du joueur. (Niveau pour l'instant) / Si ça fonctionne pas -> changer l'api key dans l'url.
         Ion.with(this)
-                .load("https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + pseudo + "?api_key=RGAPI-b763a53e-0613-44da-abc1-37aa4acd3dd0")
+                .load("https://" + strRegion + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + strSearchedUsername + "?api_key=" + API_KEY)
                 .asJsonObject()
                 .setCallback(new FutureCallback<JsonObject>() {
                     @Override
                     public void onCompleted(Exception e, JsonObject result) {
-                        TextView niveau = findViewById(R.id.tvPlayerLevel);
-                        niveau.setText("Niveau d'invocateur : " + result.get("summonerLevel").getAsString());
+                        TextView tvPlayerUsername = findViewById(R.id.tvPlayerUsername);
+                        String strPlayerUsername = result.get("name").getAsString();        // Récupération du pseudo exact (respect de la casse)
+                        tvPlayerUsername.setText(strPlayerUsername);
+
+                        ImageView ivPlayerIcon = findViewById(R.id.ivPlayerIcon);
+                        String strPlayerIcon = result.get("profileIconId").getAsString();
+                        // TODO : Changer l'icône (voir fin du README pour DATA DRAGON)
+
+                        String strPlayerLevel = result.get("summonerLevel").getAsString();
+                        TextView tvPlayerLevel = findViewById(R.id.tvPlayerLevel);
+                        tvPlayerLevel.setText("Niveau " + strPlayerLevel);
                     }
                 });
+
+        Button btnUpdatePlayerProfile = findViewById(R.id.btnUpdatePlayerProfile);
+        btnUpdatePlayerProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {   // Quand on clique sur Mettre à jour le profil
+                updatePlayerProfile();  // TODO : On appelle la méthode updatePlayerProfile()
+            }
+        });
+    }
+
+    // Fonction permettant de récupérer le nom exact de la région à mettre dans l'URL
+    public String getRegionName(String strSearchedRegion){
+        String strRegion;
+        switch(strSearchedRegion){
+            case "EUW":
+                strRegion = "euw1";
+                break;
+            case "BR":
+                strRegion = "br1";
+                break;
+            case "EUNE":
+                strRegion = "eun1";
+                break;
+            case "JP":
+                strRegion = "jp1";
+                break;
+            case "KR":
+                strRegion = "kr";
+                break;
+            case "LAN":
+                strRegion = "la1";
+                break;
+            case "LAS":
+                strRegion = "la2";
+                break;
+            case "NA":
+                strRegion = "na1";
+                break;
+            case "OCE":
+                strRegion = "oc1";
+                break;
+            case "TR":
+                strRegion = "tr1";
+                break;
+            case "RU":
+                strRegion = "ru";
+                break;
+            default:
+                strRegion = "euw1";
+                break;
+        }
+        return strRegion;
+    }
+
+    public Void updatePlayerProfile(){
+        // TODO : Implémenter la fonction
+        return null;
     }
 }
